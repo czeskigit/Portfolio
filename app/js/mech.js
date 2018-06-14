077376f7a764e174e674ee645827a28497d1e8c2
Michał Czech <michal.czech91+git@gmail.com>
2018-06-15 00:14:11 +0200

    ver 1.0

---
+++ app/js/mech.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+   
+});
+
+//  hero
+$(window).scroll(function(){
+    let scrollPosition = $(this).scrollTop();
+    let windowHeight = window.innerHeight;
+    
+    $('#hero').css('background-position', '0px ' + scrollPosition/2 +'px');
+    
+    $('#earth').css({'width':  50 + scrollPosition/20 + '%',
+                     'left': -5 + scrollPosition/100 + '%'});
+    $('#moon').css({'right': 20 + scrollPosition/20 + '%',
+                     'width': 10 - scrollPosition/50 + '%',
+                   'top': 20 + scrollPosition/10 + '%'});
+    //  navigation
+    
+    if (scrollPosition >= (windowHeight - 5)) {
+        $('#main-nav').removeClass('navbar-transp');
+        $('#main-nav').addClass('navbar-blue');
+        
+    } else {
+        $('#main-nav').addClass('navbar-transp');
+        $('#main-nav').removeClass('navbar-blue');
+    }
+    
+});
+
+//  smooth scrolling
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
