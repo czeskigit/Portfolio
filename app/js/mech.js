bf2ad373b989734fad7a235d5f540cdbb2d6ce47
Michał Czech <michal.czech91+git@gmail.com>
2018-06-20 23:22:52 +0200

    responsive correction

--- app/js/mech.js
+++ app/js/mech.js
@@ -14,6 +14,12 @@ $(window).scroll(function(){
     $('#moon').css({'right': 20 + scrollPosition/20 + '%',
                      'width': 10 - scrollPosition/50 + '%',
                    'top': 20 + scrollPosition/10 + '%'});
+    //  mobile
+    
+    $('#mobileMoon').css({'top': 25 + scrollPosition/10 + '%'});
+    $('#heroMobile').css('background-position', '0px ' + scrollPosition +'px');
+    
+    
     //  navigation
     
     if (scrollPosition >= (windowHeight - 5)) {
